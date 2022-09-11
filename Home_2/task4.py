876c7167118dc6587bf6150d4816ae512a985b23
Maxim Mineev <biton.90@mail.ru>
2022-09-11 20:31:37 +0300

    DONE

---
+++ Home_2/task4.py
@@ -0,0 +1,19 @@
+# Реализуйте алгоритм перемешивания списка.
+
+
+import random
+
+def get_list(n, lft, rght):
+    return [random.randint(lft, rght) for i in range(n)]
+
+def shuffle_list(lst):
+    return random.shuffle(lst)
+
+n = 20
+lft = -30
+rght = 50
+
+mylist = get_list(n, lft, rght)
+print(mylist)
+shuffle_list(mylist)
+print(mylist)
