9d2774c8d85b71b96eba3f2919d061e0f2a6a612
Maxim Mineev <biton.90@mail.ru>
2022-09-22 18:16:48 +0300

    Не все задачи получилось выполнить(

--- Home_4/Task1.py
+++ Home_4/Task1.py
@@ -4,7 +4,10 @@
 
 # - при $d = 0.001, π = 3.141.$    $10^{-1} ≤ d ≤10^{-10}$
 
-from math import pi
+# from math import pi
 
-d =  int(input("Введите число для заданной точности числа Пи:\n"))
-print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
+# d =  int(input("Введите число для заданной точности числа Пи:\n"))
+# print(f'число Пи с заданной точностью {d} равно {round(pi, d)}')
+id = ["id1", "id2", "id3"]
+d = list(enumerate(id=id + ))
+print(d)
